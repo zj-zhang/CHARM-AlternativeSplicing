e084aae49bb9aa70863846cc57b77b6cf74eef69
Zijun Zhang <zzj.zju@gmail.com>
2021-10-25 16:07:53 -0400

    refresh all gene AS analysis at 07

---
+++ notebooks/navy_utils.R
@@ -0,0 +1,51 @@
+# define the functions for heatmap plotting
+fill_row_with_mean = function(x)
+{
+    filled_na = 0
+    for(i in 1:nrow(x)) {
+        idx = which(is.na(x[i,]))
+        filled_na = filled_na + length(idx)
+        row_mean = mean(x[i,], na.rm=T)
+        row_mean = ifelse(is.na(row_mean), 0, row_mean)
+        x[i, idx] = row_mean
+    }
+    cat("filled_na=",filled_na,"\n")
+    return(x)
+}
+
+source("R/heatmap.3.R")
+
+plot_heatmap = function(plot_data, clab, main="heatmap.3", do_scale=T) {
+    plot_data = as.matrix(plot_data)
+    if(do_scale) {
+        y = t(scale(t(plot_data)))
+    } else {
+        y = plot_data
+    } 
+    print(dim(y))
+    y = fill_row_with_mean(y)
+
+    match_index = match(colnames(y), rownames(clab))
+    this_clab = as.matrix(clab[match_index,])
+    has_var_row = which(apply(y, 1, var)>0)
+    print(length(has_var_row))
+    y = y[has_var_row, ]
+
+    hr <- hclust(as.dist(1-cor(t(y), method="spearman", use="complete")), method="average")
+    hc <- hclust(as.dist(1-cor(y, method="spearman", use="complete")), method="average")
+    #hr <- hclust(dist(y, method="euclidean"), method="average")
+    #hc <- hclust(dist(t(y), method="euclidean"), method="average")
+
+    palette <- colorRampPalette(c("yellow3","white","darkblue"))
+
+    breaks = c(seq(-2,-1,length=100), seq(-0.99, 0.99,length=100), seq(1, 2,length=100))
+
+    print(dim(y))
+
+    heatmap.3(y, Rowv = as.dendrogram(hr), Colv = as.dendrogram(hc), dendrogram = "both", col = palette,
+      ColSideColors = this_clab, 
+      key = TRUE,
+      breaks=breaks,
+      main=main
+    )
+}
---
+++ notebooks/navy_utils.V7.py
@@ -0,0 +1,112 @@
+from jemm.covariate import Contrasts, Covariate
+import sys
+DATA_VER = sys.argv[1]
+
+# Plot style
+from matplotlib import rcParams
+from matplotlib import font_manager
+font_manager.fontManager.addfont('./data/Helvetica.ttf')
+rcParams['font.family'] = 'Helvetica'
+rcParams['axes.spines.right'] = False
+rcParams['axes.spines.top'] = False
+rcParams['xtick.top'] = False
+rcParams['ytick.right'] = False
+rcParams['figure.figsize'] = (4,4)
+rcParams['figure.dpi'] = 600
+rcParams['axes.titlesize'] = 14
+rcParams['axes.labelsize'] = 14
+rcParams['lines.linewidth'] = 1.5
+rcParams['lines.markersize'] = 8
+rcParams['xtick.labelsize'] = 14
+rcParams['ytick.labelsize'] = 14
+rcParams['legend.fontsize'] = 14
+
+try:
+    PCS_TO_INCL = sys.argv[2]
+    PCS_TO_INCL = ['PC%i'%int(i) for i in PCS_TO_INCL.split(',')]
+except:
+    PCS_TO_INCL = []
+try:
+    USE_RE = sys.argv[3]
+    USE_RE = True if USE_RE=='True' else False
+except:
+    USE_RE = False
+
+print('navy_utils.py {data} {pcs} {re}'.format(data=DATA_VER, pcs=str(PCS_TO_INCL), re=USE_RE))
+
+contrasts = Contrasts(name="final", levels=[
+    'Control',
+    'Pre',
+    'First',
+    'Mid',
+    'Post',
+    'Asymptomatic',
+    'Mild',
+    'Moderate',
+    'Exposed',
+    'False Negative',
+    'Immune',
+    'Reinfection'
+])
+covs = Covariate(fp="./%s/charm_master.clean_w_10pc.csv" % DATA_VER, sep=",", 
+                 index_col=0,
+                 contrasts=contrasts,
+                 main_effects=['final', 'pid','Sex'] + PCS_TO_INCL if USE_RE is True else ['final', 'Sex'] + PCS_TO_INCL,
+                 factor_conversion={
+                     'final': {
+                         'Asymptomatic': 'final@Asymptomatic',
+                         'Exposed': 'final@Exposed',
+                         'False Negative': 'final@False Negative',
+                         'First': 'final@First',
+                         'Immune': 'final@Immune',
+                         'Mid': 'final@Mid',
+                         'Mild': 'final@Mild',
+                         'Moderate': 'final@Moderate',
+                         'Post': 'final@Post',
+                         'Pre': 'final@Pre',
+                         'Reinfection': 'final@Reinfection'
+                     },
+                     'Sex': {'M': 'Sex@M'}
+                 },  
+                 verbose=True
+             )
+
+joint_data_files = {
+    'SE': {
+        'jct': './%s/compiled/jct_SE.txt' % DATA_VER,
+        'txr': './%s/compiled/txr_SE.txt' % DATA_VER
+    },
+    'A5SS': {
+        'jct': './%s/compiled/jct_A5SS.reduced.pkl' % DATA_VER,
+        'txr': './%s/compiled/txr_A5SS.pkl' % DATA_VER
+    },
+    'A3SS': {
+        'jct': './%s/compiled/jct_A3SS.reduced.pkl'% DATA_VER,
+        'txr': './%s/compiled/txr_A3SS.pkl'% DATA_VER
+    },
+    'RI': {
+        'jct': './%s/compiled/jct_RI.reduced.pkl'% DATA_VER,
+        'txr': './%s/compiled/txr_RI.pkl'% DATA_VER
+    }
+}
+
+joint_reg_tables = {
+    'SE': './%s/joint_SE/joint.SE.reg_table.tsv'% DATA_VER,
+    'A5SS': './%s/joint_A5SS/joint.A5SS.reg_table.tsv'% DATA_VER,
+    'A3SS': './%s/joint_A3SS/joint.A3SS.reg_table.tsv'% DATA_VER,
+    'RI': './%s/joint_RI/joint.RI.reg_table.tsv'% DATA_VER
+}
+
+m_reg_tables = {
+    'SE': './%s/gender_SE/male.SE.reg_table.tsv'% DATA_VER,
+    'A5SS': './%s/gender_A5SS/male.A5SS.reg_table.tsv'% DATA_VER,
+    'A3SS': './%s/gender_A3SS/male.A3SS.reg_table.tsv'% DATA_VER,
+    'RI': './%s/gender_RI/male.RI.reg_table.tsv'% DATA_VER
+}
+
+f_reg_tables = {
+    'SE': './%s/gender_SE/female.SE.reg_table.tsv'% DATA_VER,
+    'A5SS': './%s/gender_A5SS/female.A5SS.reg_table.tsv'% DATA_VER,
+    'A3SS': './%s/gender_A3SS/female.A3SS.reg_table.tsv'% DATA_VER,
+    'RI': './%s/gender_RI/female.RI.reg_table.tsv'% DATA_VER
+}
